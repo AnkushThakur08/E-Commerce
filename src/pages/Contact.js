cda73ee8f29ad1d3e44680f1b32ecc1061e0081e
Ankush Thakur <atpara99@gmail.com>
2021-06-09 09:33:35 +0530

    dummy data added to deploy

---
+++ src/pages/Contact.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+
+import card from "../images/card.jpeg";
+
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
+import HeroSection from "../components/Reuseable/HeroSection";
+import InfoBlock from "../components/Reuseable/InfoBlock";
+import DualInfoBlock from "../components/Reuseable/DualInfoBlock";
+import TeamImages from "../components/About/TeamImages";
+
+const AboutPage = ({ data }) => (
+  <Layout>
+    <Seo title="About" />
+    <HeroSection
+      img={data.img.childImageSharp.fluid}
+      title="About Us"
+      subtitle=""
+      heroclass="about-background"
+    />
+    <DualInfoBlock heading="Message from CEO" img={card} />
+    <InfoBlock heading="Our Vision" />
+    <TeamImages img={card} />
+  </Layout>
+);
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "about.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+          src
+        }
+      }
+    }
+  }
+`;
+
+export default AboutPage;
