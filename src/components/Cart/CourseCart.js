b156234acebf4d12836b9189881dae6e74905fb0
Ankush Thakur <atpara99@gmail.com>
2021-06-09 09:45:41 +0530

    normal stuff

--- src/components/Cart/CourseCart.js
+++ src/components/Cart/CourseCart.js
@@ -20,7 +20,10 @@ export default class CourseCart extends Component {
           <div className="row">
             {this.state.mycourses.map(({ node }) => {
               return (
-                <div key={node.id} className="col-11 col-md-6 d-flex mx-auto">
+                <div
+                  key={node.id}
+                  className="col-11 col-md-6 d-flex my-3 mx-auto"
+                >
                   <Img fixed={node.image.fixed} />
                   <div className="flex-grow-1 px-3">
                     <div className="d-flex justify-content-between">
